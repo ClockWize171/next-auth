9c4acf3cad013b9f5d730c936710c0b3d3d3605a
Min Htin <jupiterminhtin998@gmail.com>
2024-09-05 02:16:13 +0100

    change home pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button"
 
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+
 export default function Home() {
-  return (<>
-    <Button size="lg" variant="outline">Click Me!</Button>
-  </>);
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-sky-500 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6">
+        <h1 className={cn(
+          "text-6xl font-semibold text-white drop-shadow-md",
+          font.className,
+        )}>
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">
+          Simple authentication service
+        </p>
+      </div>
+    </main>
+  );
 }
