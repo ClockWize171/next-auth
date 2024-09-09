deae7539198bc51eb575054085b3785db241f553
Min Htin <jupiterminhtin998@gmail.com>
2024-09-10 00:24:59 +0100

    feat: Create User added ✅

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,32 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedField = RegisterSchema.safeParse(values);
   if (!validatedField.success) {
     return { error: "Invalid fields" };
   }
-  return { success: "Email sent!" };
+
+  const { email, password, name } = validatedField.data;
+  const hashPasswrod = await bcrypt.hash(password, 10);
+  const exsitingUser = await getUserByEmail(email);
+  if (exsitingUser) {
+    return { error: "Email already existed!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashPasswrod,
+    },
+  });
+
+  // TODO: Send Verification token mail
+
+  return { success: "User summoned!" };
 };
